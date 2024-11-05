d9bbc5a0dffd681c886ee4c1b6a896c5f6e0ad58
mahsidak <mahamud.akash70@gmail.com>
2024-11-05 20:51:10 +0600

    added a search page and widget

--- lib/app/modules/search/views/search_view.dart
+++ lib/app/modules/search/views/search_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-
+import '../../home/widget/item_github_project.dart';
 import '/app/core/widget/custom_app_bar.dart';
 import '/app/modules/bookmarks/controllers/bookmarks_controller.dart';
 import '/app/core/base/base_view.dart';
+import '/app/data/model/movie_list_response.dart';
 
 class SearchView extends BaseView<BookmarksController> {
+  final TextEditingController _searchController = TextEditingController();
+  List<MovieData> _searchResults = [];
+
   @override
   PreferredSizeWidget? appBar(BuildContext context) {
     return CustomAppBar(
@@ -12,19 +16,42 @@ class SearchView extends BaseView<BookmarksController> {
     );
   }
 
+  void _onSearch(String query) {
+    // Implement your search logic here
+    // For example, you might call a method in your controller to fetch movies based on the query
+    // setState(() { _searchResults = fetchedMovies; }); // Update the results
+    print("object");
+  }
+
   @override
   Widget body(BuildContext context) {
-    return const Center(
-      child: Text(
-        'hello world',
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _searchController,
+            onChanged: _onSearch, // Trigger search on text change
+            decoration: InputDecoration(
+              hintText: 'Search for movies...',
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Colors.grey[800],
+            ),
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-        maxLines: 2,
-        overflow: TextOverflow.ellipsis,
-      ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _searchResults.length,
+            itemBuilder: (context, index) {
+              final model = _searchResults[index];
+
+              return MovieCard(dataModel: model);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
